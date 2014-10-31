d80ba428fee3c69c8c870c2b82139d04c1c3a609
Dmitry <root@uralbash.ru>
2014-10-31 15:54:34 +0500

    divided code

--- sacrud_deform/__init__.py
+++ sacrud_deform/__init__.py
@@ -12,102 +12,19 @@ import deform
 import sqlalchemy
 from deform import Form
 from sqlalchemy import types as sa_types
-from sqlalchemy.dialects.postgresql import HSTORE, JSON
 
 from sacrud.common import get_relationship, pk_to_list
-from sacrud.exttype import ChoiceType, FileStore, GUID, SlugType
+from sacrud.exttype import ChoiceType, GUID, SlugType
 
-from .widgets import ElfinderWidget, HstoreWidget, SlugWidget
+from .common import (_get_column_type_by_sa_type, _get_widget_type_by_sa_type,
+                     _sa_row_to_choises, get_column_default_value,
+                     get_column_description, get_column_title, get_column_type,
+                     get_validator, get_widget)
 
 try:
     from pyramid_elfinder.models import ElfinderString
 except ImportError:
     ElfinderString = 'ElfinderString'
-
-
-# Map sqlalchemy types to colander types.
-_TYPES = {
-    sa_types.BigInteger: colander.Integer,
-    sa_types.Boolean: colander.Boolean,
-    sa_types.Date: colander.Date,
-    sa_types.DateTime: colander.DateTime,
-    sa_types.Enum: colander.String,
-    sa_types.Float: colander.Float,
-    sa_types.Integer: colander.Integer,
-    sa_types.Numeric: colander.Decimal,
-    sa_types.SmallInteger: colander.Integer,
-    sa_types.String: colander.String,
-    sa_types.Text: colander.String,
-    sa_types.Time: colander.Time,
-    sa_types.Unicode: colander.String,
-    sa_types.UnicodeText: colander.String,
-    JSON: colander.String,
-    HSTORE: colander.String,
-    sqlalchemy.ForeignKey: colander.String,
-    ChoiceType: colander.String,
-    FileStore: deform.FileData,
-    SlugType: colander.String,
-}
-
-# Map sqlalchemy types to deform widgets.
-_WIDGETS = {
-    sa_types.BigInteger: deform.widget.TextInputWidget,
-    sa_types.Boolean: deform.widget.CheckboxWidget,
-    sa_types.Date: deform.widget.DateInputWidget,
-    sa_types.DateTime: deform.widget.DateTimeInputWidget,
-    sa_types.Enum: deform.widget.SelectWidget,
-    sa_types.Float: deform.widget.TextInputWidget,
-    sa_types.Integer: deform.widget.TextInputWidget,
-    sa_types.Numeric: deform.widget.TextInputWidget,
-    sa_types.SmallInteger: deform.widget.TextInputWidget,
-    sa_types.String: deform.widget.TextInputWidget,
-    sa_types.Text: deform.widget.TextAreaWidget,
-    sa_types.Time: deform.widget.TextInputWidget,
-    sa_types.Unicode: deform.widget.TextInputWidget,
-    sa_types.UnicodeText: deform.widget.TextAreaWidget,
-    JSON: deform.widget.TextAreaWidget,
-    HSTORE: HstoreWidget,
-    sqlalchemy.ForeignKey: deform.widget.SelectWidget,
-    ChoiceType: deform.widget.SelectWidget,
-    FileStore: deform.widget.FileUploadWidget,
-    ElfinderString: ElfinderWidget,
-    SlugType: SlugWidget,
-}
-
-
-def _sa_row_to_choises(rows):
-    return [(getattr(ch, pk_to_list(ch)[0]), ch.__repr__()) for ch in rows]
-
-
-def _get_column_type_by_sa_type(sa_type):
-    """
-    Returns the colander type that correspondents to the sqlalchemy type
-    'sa_type'.
-    """
-    return _TYPES.get(sa_type) or colander.String
-
-
-def _get_widget_type_by_sa_type(sa_type):
-    """
-    Returns the deform widget that correspondents to the sqlalchemy type
-    'sa_type'.
-    """
-    return _WIDGETS.get(sa_type) or deform.widget.TextInputWidget
-
-
-def get_validator(widget):
-    return colander.All()
-
-
-class HTMLText(object):
-    def __init__(self, text):
-        self.text = text
-
-    def __html__(self):
-        try:
-            return unicode(self.text)
-        except NameError:           # pragma: no cover
-            return str(self.text)   # pragma: no cover
 
 
 class GroupShema(object):
@@ -121,24 +38,6 @@ class GroupShema(object):
         self.schema = colander.Schema(name=translate(group))
         self.build(columns)
 
-    def get_column_title(self, col):
-        if 'verbose_name' in col.info:
-            name = col.info['verbose_name']
-        else:
-            name = col.name
-        if 'sacrud_position' in col.info:
-            if col.info['sacrud_position'] == 'inline':
-                if 'verbose_name' in col.info:
-                    name = col.info['verbose_name']
-                else:
-                    name = col.sacrud_name
-        return self.translate(name)
-
-    def get_column_description(self, col):
-        if 'description' in col.info:
-            return HTMLText(col.info['description'])
-        return None
-
     def get_column_css_styles(self, col):
         css_class = ['sacrud_deform', ]
         if hasattr(self.table, 'sacrud_css_class'):
@@ -147,40 +46,6 @@ class GroupShema(object):
                     css_class.append(key)
             return ' '.join(css_class)
         return None
-
-    def get_column_type(self, col):
-        if hasattr(col, 'type'):
-            return col.type.__class__
-        return None
-
-    def get_col_default_value(self, col, obj):
-        value = None
-        col_type = self.get_column_type(col)
-        if obj and hasattr(col, 'instance_name'):
-            value = getattr(obj, col.instance_name)
-        elif obj and hasattr(col, 'name'):
-            try:
-                value = getattr(obj, col.name, colander.null)
-            except UnicodeEncodeError:
-                value = colander.null
-        if value is None:
-            value = colander.null
-        elif col_type == ChoiceType:
-            value = value[0]
-        if col_type == sa_types.Boolean:
-            value = bool(value)
-        return value
-
-    def get_widget(self, widget_type, values, mask, css_class,
-                   col):
-        if widget_type == deform.widget.FileUploadWidget:
-            return widget_type(None)
-        return widget_type(values=values,
-                           mask=mask,
-                           col=col,
-                           mask_placeholder='_',
-                           css_class=css_class,
-                           )
 
     def get_node(self, values=None, mask=None, validator=None,
                  node_kwargs={}, **kwargs):
@@ -197,9 +62,9 @@ class GroupShema(object):
             self.js_list.append('pyramid_elfinder:static/js/proxy/elFinderSupportVer1.js')
         if kwargs['sa_type'] == SlugType:
             self.js_list.append('pyramid_sacrud:static/js/lib/speakingurl.min.js')
-        widget = self.get_widget(widget_type, values, mask,
-                                 kwargs['css_class'],
-                                 kwargs['col'])
+        widget = get_widget(widget_type, values, mask,
+                            kwargs['css_class'],
+                            kwargs['col'])
         validator = get_validator(widget)
         if widget_type == deform.widget.FileUploadWidget:
             kwargs['description'] = kwargs['default']
@@ -254,11 +119,11 @@ class GroupShema(object):
                 self.schema.add(m2m)
                 continue
 
-            title = self.get_column_title(col)
-            default = self.get_col_default_value(col, self.obj)
-            description = self.get_column_description(col)
+            title = get_column_title(col, self.translate)
+            default = get_column_default_value(col, self.obj)
+            description = get_column_description(col)
             css_class = self.get_column_css_styles(col)
-            sa_type = self.get_column_type(col)
+            sa_type = get_column_type(col)
             params = {'col': col,
                       'sa_types': sa_type,
                       'title': title,
