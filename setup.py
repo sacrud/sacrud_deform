b239511fe0268b3767fa0992c06acaedcef74733
Dmitry <root@uralbash.ru>
2015-02-04 17:02:28 +0500

    ++version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(name):
 
 setup(
     name='sacrud_deform',
-    version="0.0.5",
+    version="0.0.6",
     url='http://github.com/ITCase/sacrud_deform/',
     author='Svintsov Dmitry',
     author_email='root@uralbash.ru',
