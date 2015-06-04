626321e30679159b73f322552cfaafce6430381f
Dmitry <root@uralbash.ru>
2015-06-04 15:45:33 +0500

    fix theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,9 +16,6 @@ intersphinx_mapping = {
     'sacrud': ('http://sacrud.readthedocs.org/en/latest/', None),
 }
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
 # The suffix of source filenames.
 source_suffix = '.rst'
 
